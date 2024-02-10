dca136b41ca5a2700e1d1da8ca2965e4e201aeaa
Douglas Vidal <vidaldl01@gmail.com>
2024-02-10 18:18:09 -0300

    feat. add check if title is on favorites or is not on favorites when adding or removing from favorites

--- src/app/api/addToFavorites/route.ts
+++ src/app/api/addToFavorites/route.ts
@@ -3,6 +3,7 @@ import User, { IUser } from "@/app/db/model";
 import { HydratedDocument } from "mongoose";
 import { NextResponse, type NextRequest } from "next/server.js";
 
+// TODO: verificar se o userId da sessao é o mesmo que irá adicionar aos favoritos
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const { id, imdbID }: { id: string; imdbID: string } = await req.json();
@@ -24,11 +25,22 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
       return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
 
+    const isTitleAlreadyFavorite: boolean = user.favorites.some(
+      (favorite) => favorite.imdbID === imdbID
+    );
+
+    if (isTitleAlreadyFavorite) {
+      return NextResponse.json(
+        { message: "Title already in favorites" },
+        { status: 400 }
+      );
+    }
+
     user.favorites.push({ imdbID: imdbID });
 
     await user.save();
 
-    console.log(`Title: ${imdbID} added to User: ${id} favorites`);
+    console.log(`Title: ${imdbID} added to User: ${id}'s favorites`);
 
     await db.disconnect();
 
---
+++ src/app/api/removeFromFavorites/route.ts
@@ -0,0 +1,55 @@
+import db from "@/app/db/db";
+import User, { IUser } from "@/app/db/model";
+import { HydratedDocument } from "mongoose";
+import { NextResponse, type NextRequest } from "next/server.js";
+
+// TODO: verificar se o userId da sessao é o mesmo que irá adicionar aos favoritos
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const { id, imdbID }: { id: string; imdbID: string } = await req.json();
+
+    if (!id || !imdbID) {
+      return NextResponse.json(
+        { message: "Missing information" },
+        { status: 400 }
+      );
+    }
+
+    await db.connect();
+
+    const user = (await User.findOne({
+      externalId: id,
+    })) as HydratedDocument<IUser>;
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const index = user.favorites.findIndex(
+      (favorite) => favorite.imdbID === imdbID
+    );
+
+    if (index === -1) {
+      return NextResponse.json(
+        { message: "Title is not in favorites" },
+        { status: 400 }
+      );
+    }
+
+    user.favorites.splice(index, 1);
+
+    await user.save();
+
+    console.log(`Title: ${imdbID} removed from User: ${id}'s favorites`);
+
+    await db.disconnect();
+
+    return NextResponse.json(
+      { message: `Title: ${imdbID} added to User: ${id} favorites` },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.log(error.message);
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
