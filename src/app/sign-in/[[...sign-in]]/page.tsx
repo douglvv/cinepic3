ccc5ae0054a87bd1f41083b54acf5f871b580e15
Douglas Vidal <vidaldl01@gmail.com>
2024-02-17 16:45:16 -0300

    feat. add udpate style sign-in/sign-up forms

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -6,8 +6,14 @@ export default function Page() {
     <div className="min-h-[90vh] container flex mt-12 justify-center ">
       <SignIn
         redirectUrl={"/"}
-        appearance={{ baseTheme: dark }}
-        afterSignInUrl={'/browse'}
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            formButtonPrimary:
+              "bg-red-800 hover:bg-red-900 active:bg-red-950 active:shadow-inner text-neutral-300 hover:text-neutral-200 font-semibold",
+          },
+        }}
+        afterSignInUrl={"/browse"}
       />
     </div>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -5,9 +5,15 @@ export default function Page() {
   return (
     <div className="min-h-[90vh] container flex mt-12 justify-center ">
       <SignUp
-        appearance={{ baseTheme: dark }}
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            formButtonPrimary:
+              "bg-red-800 hover:bg-red-900 active:bg-red-950 active:shadow-inner text-neutral-300 hover:text-neutral-200 font-semibold",
+          },
+        }}
         redirectUrl={"/"}
-        afterSignUpUrl={'/browse'}
+        afterSignUpUrl={"/browse"}
       />
     </div>
   );
